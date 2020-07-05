6fd8a7c54b36be24e3baa62ba0259d3e0a481356
ihays <64121529+ihays@users.noreply.github.com>
2020-07-05 12:04:48 -0700

    Create SetMatrixZeroes.cpp

---
+++ SetMatrixZeroes.cpp
@@ -0,0 +1,37 @@
+//Ian Hays
+//07-05-2020
+//https://leetcode.com/problems/set-matrix-zeroes/
+//SC: O(M+N) TC: O(MN)
+
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        vector<int> rows(m,0);
+        vector<int> cols(n,0);
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(matrix[i][j] == 0){
+                    rows[i] = 1;
+                    cols[j] = 1;
+                }
+            }
+        }
+        for(int i = 0; i < m; i++){
+            if(rows[i] == 1){
+                for(int j = 0; j < n; j++){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        
+        for(int i = 0; i < n; i++){
+            if(cols[i] == 1){
+                for(int j = 0; j < m; j++){
+                    matrix[j][i] = 0;
+                }
+            }
+        }        
+    }
+};
