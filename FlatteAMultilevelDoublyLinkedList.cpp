6624c56b961490a09ab5f248f7a4522b03141536
ihays <64121529+ihays@users.noreply.github.com>
2020-07-10 12:37:54 -0700

    Create FlatteAMultilevelDoublyLinkedList.cpp

---
+++ FlatteAMultilevelDoublyLinkedList.cpp
@@ -0,0 +1,42 @@
+//Ian Hays
+//07-10-2020
+//https://leetcode.com/problems/flatten-a-multilevel-doubly-linked-list/
+//SC: O(1) TC: O(N)
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* prev;
+    Node* next;
+    Node* child;
+};
+*/
+
+class Solution {
+public:
+    Node* flatten(Node* head) {
+
+        while(true){
+            Node* tail = head;
+            while(tail && !tail->child){
+                tail = tail->next;
+            }
+            if(!tail) break;            
+            
+            Node* tmp = tail->next; 
+            tail->next = tail->child;
+            tail->child->prev = tail;
+            tail->child = NULL;
+            
+            while(tail->next){
+                tail = tail->next;
+            }
+            tail->next = tmp;
+            if(tmp) tmp->prev = tail;
+        }
+
+        return head;
+    }
+};
