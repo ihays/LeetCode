7660266465d700d8fe0ce5b82be71ef2b2d5f0e1
ihays <64121529+ihays@users.noreply.github.com>
2020-07-11 10:16:25 -0700

    Create ApplesAndOranges.sql

---
+++ ApplesAndOranges.sql
@@ -0,0 +1,11 @@
+//Ian Hays
+//07-11-2020
+//https://leetcode.com/problems/apples-oranges/
+
+# Write your MySQL query statement below
+SELECT s.sale_date,
+       (select sum(sold_num) from Sales where sale_date = s.sale_date and fruit = 'apples')-
+       (select sum(sold_num) from Sales where sale_date = s.sale_date and fruit = 'oranges') as diff
+
+FROM Sales s
+group by s.sale_date
