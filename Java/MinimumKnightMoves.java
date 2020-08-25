9c89fb158776f8298a151d95f7c8f6a7a5320b77
ihays <64121529+ihays@users.noreply.github.com>
2020-08-24 22:03:51 -0700

    Create MinimumKnightMoves.java

---
+++ Java/MinimumKnightMoves.java
@@ -0,0 +1,39 @@
+class Solution {
+    private final int[][] directions = new int[][] {{1,2},{-1,2},{1,-2},{-1,-2},{2,1},{-2,1},{2,-1},{-2,-1}};
+    
+    public int minKnightMoves(int x, int y) {
+        Set<String> visited = new HashSet<>();
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[] {0,0});
+        int res = 0;
+        x = Math.abs(x);
+        y = Math.abs(y);
+        while(!queue.isEmpty()){
+            boolean flag = false;
+            int m = queue.size();
+            for(int i = 0; i < m; i++){
+                int[] position = queue.remove();
+                int X = position[0];
+                int Y = position[1];
+                if (X < -2 || Y < -2) continue;
+                if (X > x+2 || Y > y+2) continue;
+                if(X == x && Y == y){
+                    flag = true;
+                    break;
+                } 
+                for(int j = 0; j < directions.length; j++){
+                    int tmpX = X + directions[j][0];
+                    int tmpY = Y + directions[j][1];
+                    if(!visited.contains(tmpX + "," + tmpY)){
+                        visited.add(tmpX + "," + tmpY);
+                        queue.add(new int[] {tmpX,tmpY});
+                    }
+                    
+                }
+            }
+            if(flag) break;
+            res++;
+        }
+        return res;
+    }
+}
