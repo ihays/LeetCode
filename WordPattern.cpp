bdc43e08f02ad59d28bf8cb64b0b9c68e63c332e
ihays <64121529+ihays@users.noreply.github.com>
2020-09-08 16:49:09 -0700

    Update WordPattern.cpp

--- WordPattern.cpp
+++ WordPattern.cpp
@@ -8,11 +8,10 @@ public:
     bool wordPattern(string pattern, string str) {
         unordered_map<string,char> umap1;
         unordered_map<char,string> umap2;
-        int begin = 0;
-        int end = 0;
-        for(int i = 0; i < pattern.length(); i++){
-            end = str.find(' ', begin);
-            string tmp = str.substr(begin,end-begin);
+        istringstream in(str);
+        int i = 0;
+        
+        for(string tmp; in >> tmp; i++){
             if(!umap1[tmp]){
                 umap1[tmp] = pattern[i];
             } else if (umap1[tmp] != pattern[i]){
@@ -24,9 +23,8 @@ public:
             } else if (umap2[pattern[i]] != tmp){
                 return false;
             }
-            begin = end + 1;
         }
-        if(end != string::npos) return false;
-        return true;
+        
+        return i == pattern.length();
     }
 };
