9ac0cec5c824bd34c3dba19291133f2f13194bcf
ihays <64121529+ihays@users.noreply.github.com>
2020-12-11 10:19:21 -0800

    Create MinimumNumberOfVerticesToReachAllNodes.cpp

---
+++ C++/MinimumNumberOfVerticesToReachAllNodes.cpp
@@ -0,0 +1,21 @@
+//Ian Hays
+//12-11-20
+//https://leetcode.com/problems/minimum-number-of-vertices-to-reach-all-nodes/
+//SC: O(N^2) TC: O(N^2)
+
+class Solution {
+public:
+    vector<int> findSmallestSetOfVertices(int n, vector<vector<int>>& edges) {
+        vector<int> indegrees = vector<int>(n,0);
+        vector<int> res;
+        for(int i = 0; i < edges.size(); i++){
+            indegrees[edges[i][1]]++;
+        }
+        
+        for(int i = 0; i < indegrees.size(); i++){
+            if(indegrees[i] == 0) res.push_back(i);
+        }
+        
+        return res;
+    }
+};
