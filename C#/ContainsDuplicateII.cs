68ef6694518c61957fa5c021757deb1f77bacfd1
ihays <64121529+ihays@users.noreply.github.com>
2020-12-24 11:29:08 -0800

    Create ContainsDuplicateII.cs

---
+++ C#/ContainsDuplicateII.cs
@@ -0,0 +1,15 @@
+//Ian Hays
+//12-24-2020
+//https://leetcode.com/problems/contains-duplicate-ii/
+//SC: O(N) TC: O(N)
+
+public class Solution {
+    public bool ContainsNearbyDuplicate(int[] nums, int k) {
+        Dictionary<int,int> dictionary = new Dictionary<int,int>();
+        for(int i = 0; i < nums.Length; i++){
+            if(dictionary.ContainsKey(nums[i]) && i - dictionary[nums[i]] <= k) return true;
+            dictionary[nums[i]] = i;
+        }
+        return false;
+    }
+}
