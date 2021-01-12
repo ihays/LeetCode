658615eaf54fd8f269dffd014f462078fa2c81b6
ihays <64121529+ihays@users.noreply.github.com>
2021-01-11 17:03:24 -0800

    Update MergeSortedArray.cpp

--- C++/MergeSortedArray.cpp
+++ C++/MergeSortedArray.cpp
@@ -1,32 +1,31 @@
 //Ian Hays
-//07-02-2020
+//01-11-2021
 //https://leetcode.com/problems/merge-sorted-array/
 //SC: O(1) TC: O(N*LOG(N))
 //Binary search to insert values from nums2 into nums1
+//updated on 01-11-2021 to resize first and then insert to reduce space/time. 
 
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         nums1.resize(m);
-        for(int i = 0; i < nums2.size(); i++){
-            nums1.insert(nums1.begin()+binarySearch(nums1, 0, m-1, nums2[i]),nums2[i]);
-            m++;
+        for(auto it: nums2){
+            nums1.insert(nums1.begin() + binarySearch(nums1, it), it);
         }
     }
     
-    int binarySearch(vector<int> nums1, int l, int r, int x) { 
-        int mid =  l + (r - l) / 2;
-        if (l <= r) { 
-            mid = l + (r - l) / 2; 
-            if (nums1[mid] == x) 
-                return mid; 
-
-            if (nums1[mid] > x){
-                return binarySearch(nums1, l, mid - 1, x); 
+    int binarySearch(vector<int>& nums1, int target){
+        int left = 0;
+        int right = nums1.size();
+        
+        while(left < right){
+            int mid = (left+right)/2;
+            if(target <= nums1[mid]){
+                right = mid;
             } else {
-                return binarySearch(nums1, mid + 1, r, x); 
-            } 
-        } 
-        return mid; 
-    } 
+                left = mid+1;
+            }   
+        }
+        return left;
+    }
 };
