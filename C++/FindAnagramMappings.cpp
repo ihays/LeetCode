d16237b7d5fcf369a10c4e0dfe7a8ea3a920511f
ihays <64121529+ihays@users.noreply.github.com>
2021-01-16 11:38:39 -0800

    Update FindAnagramMappings.cpp

--- C++/FindAnagramMappings.cpp
+++ C++/FindAnagramMappings.cpp
@@ -1,22 +1,15 @@
 //Ian Hays
-//07-12-2020
+//01-16-2021
 //https://leetcode.com/problems/find-anagram-mappings/
 //SC: O(N) TC: O(N)
 
 class Solution {
 public:
     vector<int> anagramMappings(vector<int>& A, vector<int>& B) {
-        unordered_map<int,int> umap;
         vector<int> res;
-        
-        for(int i = 0; i < B.size(); i++){
-            umap[B[i]] = i;
-        }
-        
-        for(int i = 0; i < A.size(); i++){
-            res.push_back(umap[A[i]]);
-        }
-        
+        unordered_map<int,int> umap;
+        for(int i = 0; i < B.size(); i++) umap[B[i]] = i;
+        for(auto &it: A) res.push_back(umap[it]);
         return res;
     }
 };
