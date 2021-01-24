7054826c12b65d4c0a511f6537984e1fe3259200
ihays <64121529+ihays@users.noreply.github.com>
2021-01-24 10:55:53 -0800

    Create MergeKSortedLists.cpp

---
+++ C++/MergeKSortedLists.cpp
@@ -0,0 +1,41 @@
+//Ian Hays
+//01-24-2021
+//https://leetcode.com/problems/merge-k-sorted-lists/
+//SC: O(N) TC: O(N)
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        priority_queue<int, vector<int>, greater<int>> q;
+        for(int i = 0; i < lists.size(); i++){
+            ListNode* l = lists[i];
+            while(l){
+                q.push(l->val);
+                l = l->next;
+            }
+        }
+        if(q.empty()) return nullptr;
+        
+        ListNode dummy;
+        ListNode* tail = new ListNode(q.top());
+        dummy.next = tail;
+        q.pop();
+        
+        while(!q.empty()){
+            tail->next = new ListNode(q.top());
+            tail = tail->next;
+            q.pop();
+        }
+        return dummy.next;
+    }
+};
