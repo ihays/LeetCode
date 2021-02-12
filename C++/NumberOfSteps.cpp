d5ae7bd2752b1c3ab7ebf0aab7ec0c6053c07a80
ihays <64121529+ihays@users.noreply.github.com>
2021-02-12 07:18:29 -0800

    Update NumberOfSteps.cpp

--- C++/NumberOfSteps.cpp
+++ C++/NumberOfSteps.cpp
@@ -6,6 +6,6 @@
 class Solution {
 public:
     int numberOfSteps (int num) {
-        return !num ? 0 : 1 + (num%2 ? numberOfSteps(num-1) : numberOfSteps(num/2));
+        return !num ? 0 : 1 + numberOfSteps(num%2 ? num-1 : num/2);
     }
 };
