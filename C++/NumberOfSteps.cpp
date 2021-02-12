5d73703f0651c03e05c8d0a97ab87b5544368a3d
ihays <64121529+ihays@users.noreply.github.com>
2021-02-12 07:12:21 -0800

    Update NumberOfSteps.cpp

--- C++/NumberOfSteps.cpp
+++ C++/NumberOfSteps.cpp
@@ -1,12 +1,11 @@
 //Ian Hays
-//07-11-2020
+//02-12-21
 //https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/
-//SC: O(1) TC: O(N)
+//SC: O(1) TC: O(N) - 1 liner, recursive
 
 class Solution {
 public:
     int numberOfSteps (int num) {
-        if(!num) return 0;
-        return 1 + (num%2 ? numberOfSteps(num-1) : numberOfSteps(num/2));
+        return !num ? 0 : 1 + (num%2 ? numberOfSteps(num-1) : numberOfSteps(num/2));
     }
 };
