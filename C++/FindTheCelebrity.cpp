f0a15b8f8242ddc80290c321c3424a9f17bdd171
ihays <64121529+ihays@users.noreply.github.com>
2021-02-26 09:02:44 -0800

    Update FindTheCelebrity.cpp

--- C++/FindTheCelebrity.cpp
+++ C++/FindTheCelebrity.cpp
@@ -1,7 +1,14 @@
-//Ian Hays
-//02-26-2021
-//https://leetcode.com/problems/find-the-celebrity/
-//SC: O(N) TC: O(1)
+/***********************************************************************************************
+Problem      Find The Celebrity
+Developer    Ian Hays
+Date         02/26/2021
+URL          https://leetcode.com/problems/find-the-celebrity/
+Space        O(N) 
+Time         O(1)
+Description  Dfs through each person based on who they know. The celebrity candidate should always 
+             be the end point since they do not know anyone. We then verify the celebrity to make
+             sure everyone knows them, and they know no one.             
+************************************************************************************************/
 
 /* The knows API is defined for you.
       bool knows(int a, int b); */
@@ -10,10 +17,11 @@ class Solution {
 public:
     int findCelebrity(int n) {
         int person = 0;
+        //dfs through each person based on who they know
         for(int i = 1; i < n; i++){
             if(knows(person,i)) person = i;
         }
-                
+        //verify person is the celebrity
         for(int i = 0; i < n; i++){
             if(person == i) continue;
             if(!knows(i,person) || knows(person,i)) return -1;
