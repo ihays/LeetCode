46bdcb184cbcdef8b428f97c7f35ccede3efef91
ihays <64121529+ihays@users.noreply.github.com>
2021-03-23 21:16:31 -0700

    Update FindSmallestCommonElementInAllRows.cpp

--- C++/FindSmallestCommonElementInAllRows.cpp
+++ C++/FindSmallestCommonElementInAllRows.cpp
@@ -1,16 +1,21 @@
-//Ian Hays
-//12-09-20
-//https://leetcode.com/problems/find-smallest-common-element-in-all-rows/
-//SC: O(1) TC: O(N^2)
+/***********************************************************************************************
+Problem      Find Smallest Common Element in All Rows
+Developer    Ian Hays
+Date         03/23/2021
+URL          https://leetcode.com/problems/find-smallest-common-element-in-all-rows/
+Space        O(1) 
+Time         O(N^2)
+Description  Increment value in map. Return first value to reach the size of all rows.            
+************************************************************************************************/
 
 class Solution {
 public:
     int smallestCommonElement(vector<vector<int>>& mat) {
-        vector<int> vect(10001);
-        for(int i = 0; i < mat.size(); i++){
-            for(int j = 0; j < mat[i].size(); j++){
-                if(++vect[mat[i][j]] == mat.size()) return mat[i][j];
-                
+        int map[10000] = {0};
+        int n = mat.size(), m = mat[0].size();
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(++map[mat[i][j]] == n) return mat[i][j];
             }
         }
         return -1;
