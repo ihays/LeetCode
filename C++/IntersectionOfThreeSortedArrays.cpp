32e3420110fcceea81c617dd024dc2a82cf5f0a3
ihays <64121529+ihays@users.noreply.github.com>
2021-05-18 22:17:44 -0700

    Update IntersectionOfThreeSortedArrays.cpp

--- C++/IntersectionOfThreeSortedArrays.cpp
+++ C++/IntersectionOfThreeSortedArrays.cpp
@@ -1,23 +1,29 @@
-//Ian Hays
-//07-12-2020
-//https://leetcode.com/problems/intersection-of-three-sorted-arrays/
-//SC: O(1) TC: O(N)
+/***********************************************************************************************
+Problem      Intersection of Three Sorted Arrays
+Developer    Ian Hays
+Date         05/18/2021
+URL          https://leetcode.com/problems/intersection-of-three-sorted-arrays/
+Space        O(N) 
+Time         O(N)
+Description  Store quantities of values from arrays in a map. Quantities of 3 are an intersection
+             and pushed into the result. 
+************************************************************************************************/
 
 class Solution {
 public:
     vector<int> arraysIntersection(vector<int>& arr1, vector<int>& arr2, vector<int>& arr3) {
-        unordered_map<int,int> umap;
-        vector<int> res;
+        map<int,int> map;
         
         for(int i = 0; i < arr1.size(); i++){
-            umap[arr1[i]]++;
-            umap[arr2[i]]++;
-            umap[arr3[i]]++;
+            map[arr1[i]]++;
+            map[arr2[i]]++;
+            map[arr3[i]]++;
         }
         
-        for(int i = 0; i < arr1.size(); i++){
-            if(umap[arr1[i]] == 3){
-                res.push_back(arr1[i]);
+        vector<int> res;
+        for(auto it: map){
+            if(it.second == 3){
+                res.push_back(it.first);
             }
         }
         return res;
