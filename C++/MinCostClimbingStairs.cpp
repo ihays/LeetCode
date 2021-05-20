e0930959012881cbba125924eb879af6e5f17ab0
ihays <64121529+ihays@users.noreply.github.com>
2021-05-20 10:00:25 -0700

    Create MinCostClimbingStairs.cpp

---
+++ C++/MinCostClimbingStairs.cpp
@@ -0,0 +1,21 @@
+/***********************************************************************************************
+Problem      Min Cost Climbing Stairs
+Developer    Ian Hays
+Date         05/20/2021
+URL          https://leetcode.com/problems/min-cost-climbing-stairs/
+Space        O(1) 
+Time         O(N)
+Description  Use cost vector as an in place DP. We grab the minimum cost between 1 & 2 steps back
+             and add to current cost. We return the cheapest cost between the last & second to 
+             last step. 
+************************************************************************************************/
+
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        for(int i = 2; i < cost.size(); i++){
+            cost[i] += min(cost[i-1], cost[i-2]);
+        }
+        return min(cost[cost.size()-1], cost[cost.size()-2]);;
+    }
+};
