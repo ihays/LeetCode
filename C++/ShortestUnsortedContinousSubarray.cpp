0d340115d89de4088822466d80bf94cc41bbe2df
ihays <64121529+ihays@users.noreply.github.com>
2021-05-26 16:09:42 -0700

    Update ShortestUnsortedContinousSubarray.cpp

--- C++/ShortestUnsortedContinousSubarray.cpp
+++ C++/ShortestUnsortedContinousSubarray.cpp
@@ -1,28 +1,24 @@
-//Ian Hays
-//02-25-2021
-//https://leetcode.com/problems/shortest-unsorted-continuous-subarray/
-//SC: O(N) TC: O(1)
+/***********************************************************************************************
+Problem      Shortest Unsorted Continuous Subarray
+Developer    Ian Hays
+Date         05/26/2021
+URL          https://leetcode.com/problems/shortest-unsorted-continuous-subarray/submissions/
+Space        O(N) 
+Time         O(NLOGN)
+Description  Sort array, find first index where values are different from beginning and end (i and j).
+             We return the difference. If j ever passes i, we break the second loop so the difference
+             is 0. 
+************************************************************************************************/
 
 class Solution {
 public:
     int findUnsortedSubarray(vector<int>& nums) {
-        int l = 0;
-        int r = nums.size()-1;
-        while(l+1 < nums.size() && nums[l] <= nums[l+1]) l++;
-        while(r-1 >= 0 && nums[r] >= nums[r-1]) r--;
-        
-        if(r <= l) return 0;
-        int minimum = INT_MAX;
-        int maximum = INT_MIN;
-       
-        for(int i = l; i <= r; i++){
-            minimum = min(nums[i], minimum);
-            maximum = max(nums[i], maximum);
-        }
-        
-        while(l-1 >= 0 && nums[l-1] > minimum) l--;
-        while(r+1 < nums.size() && nums[r+1] < maximum) r++;
-        
-        return r-l+1;
+        vector<int> sorted(nums);
+        sort(nums.begin(), nums.end());
+        int i = 0;
+        while(i < nums.size() && sorted[i] == nums[i]) i++;
+        int j = nums.size()-1;
+        while(j > i && sorted[j] == nums[j]) j--;
+        return j-i+1;
     }
 };
