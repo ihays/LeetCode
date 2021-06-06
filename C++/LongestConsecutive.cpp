ee2f21c2b03fafedd968f8e3175dfd02da084b7a
ihays <64121529+ihays@users.noreply.github.com>
2021-06-06 09:46:19 -0700

    Update LongestConsecutive.cpp

--- C++/LongestConsecutive.cpp
+++ C++/LongestConsecutive.cpp
@@ -1,34 +1,27 @@
-//Ian Hays
-//09-16-2020
-//https://leetcode.com/problems/longest-consecutive-sequence/
-//SC: O(N) TC: O(N)
+/***********************************************************************************************
+Problem      Longest Consecutive Sequence
+Developer    Ian Hays
+Date         06/06/2021
+URL          https://leetcode.com/problems/longest-consecutive-sequence/
+Space        O(N)
+Time         O(N)
+Description  Place all values into a set for O(1) access. We loop through all numbers until we find
+             a number that has no previous value. We count all numbers following in sequence that 
+             exist in the set. Return the maximum sequence.
+************************************************************************************************/
 
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        if(nums.empty()) return 0;
-        priority_queue<int> pq;
-        for(int num : nums){
-            pq.push(num);
-        }
-        int count = 1, count_max = 1;
-        int prev = pq.top();
-        pq.pop();
-
-        while(!pq.empty()){
-            int curr = pq.top();
-            if(curr + 1 == prev){
-                count++;
-                count_max = max(count_max,count);
-            } else if(curr == prev) {
-                pq.pop();
-                continue;
-            } else {
-                count = 1;
+        unordered_set<int> uset(begin(nums), end(nums));
+        int res = 0;
+        for(auto num: nums){
+            if(!uset.count(num-1)){
+                int count = 1;
+                while(uset.count(num+count)) count++;
+                res = max(res,count);    
             }
-            prev = curr;
-            pq.pop();
         }
-        return count_max;
+        return res;
     }
 };
