54202a59baf39529745f24813ed4cacd11aa9a9e
ihays <64121529+ihays@users.noreply.github.com>
2021-06-07 16:50:50 -0700

    Update MinCostClimbingStairs.cpp

--- C++/MinCostClimbingStairs.cpp
+++ C++/MinCostClimbingStairs.cpp
@@ -16,6 +16,6 @@ public:
         for(int i = 2; i < cost.size(); i++){
             cost[i] += min(cost[i-1], cost[i-2]);
         }
-        return min(cost[cost.size()-1], cost[cost.size()-2]);;
+        return min(cost[cost.size()-1], cost[cost.size()-2]);
     }
 };
