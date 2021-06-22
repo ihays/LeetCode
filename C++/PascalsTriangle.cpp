1f4ec2950254619fb54970367dce367d123f6955
ihays <64121529+ihays@users.noreply.github.com>
2021-06-21 21:10:12 -0700

    Update PascalsTriangle.cpp

--- C++/PascalsTriangle.cpp
+++ C++/PascalsTriangle.cpp
@@ -1,24 +1,23 @@
-//Ian Hays
-//07-05-2020
-//https://leetcode.com/problems/pascals-triangle/
-//SC: O(N^2) TC: O(N^2)
-
+/***********************************************************************************************
+Problem      Pascals Triangle
+Developer    Ian Hays
+Date         06/21/2021
+URL          https://leetcode.com/problems/pascals-triangle/
+Space        O(N^2) 
+Time         O(N^2)
+Description  Build using pascal formula storing in a result vector. 
+************************************************************************************************/
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> res;
-        if(!numRows) return res;
-        res.push_back(vector<int>{1});
-        int row = 1;
-        while(numRows!=row){
-            res.push_back(vector<int>({1,1}));
-            for(int i = 1; i < res[row-1].size(); i++){
-                res[row].insert(res[row].begin()+i, res[row-1][i-1] + res[row-1][i]);
-            }
-            row++;
+        vector<vector<int>> res(1, vector<int>(1,1));
+        while(--numRows){
+            vector<int> curr(1,1);
+            for(int j = 1; j < size(res.back()); j++)
+                curr.emplace_back(res.back()[j-1] + res.back()[j]);
+            curr.emplace_back(1);
+            res.emplace_back(move(curr));
         }
-        
         return res;
-        
     }
 };
