ede07a3640a225579d751636d466724c8f77d23b
ihays <64121529+ihays@users.noreply.github.com>
2021-07-12 10:55:15 -0700

    Update IsomorphicStrings.cpp

--- C++/IsomorphicStrings.cpp
+++ C++/IsomorphicStrings.cpp
@@ -1,29 +1,27 @@
-//Ian Hays
-//07-10-2020
-//https://leetcode.com/problems/isomorphic-strings/
-//SC: O(N) TC: O(N)
+/***********************************************************************************************
+Problem      Isomorphic Strings
+Developer    Ian Hays
+Date         07/12/2021
+URL          https://leetcode.com/problems/isomorphic-strings/
+Space        O(N)
+Time         O(N)
+Description  Use mapping array to determine if strings relations biject 1 to 1.
+************************************************************************************************/
 
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        unordered_map<char,char> umap;
-        
-        for(int i = 0; i < s.size(); i++){
-            if(umap.find(s[i]) == umap.end()){
-                umap[s[i]] = t[i];
-            } else if (umap[s[i]] != t[i]){
-                return false;
-            }
-        }
-        umap.clear();
-        for(int i = 0; i < s.size(); i++){
-            if(umap.find(t[i]) == umap.end()){
-                umap[t[i]] = s[i];
-            } else if (umap[t[i]] != s[i]){
-                return false;
-            }
-        }
-        
-        return true;
+        return normalize(s,t) && normalize(t,s);
     }
+    bool normalize(string a, string b){
+        char mapping[128] = {};
+        for(int i = 0; i < size(a); i++){
+            if(mapping[a[i]] == 0)
+                mapping[a[i]] = b[i];    
+            else if(mapping[a[i]] != b[i]) return false;
+        }
+        return true;
+    }  
 };
+
+
