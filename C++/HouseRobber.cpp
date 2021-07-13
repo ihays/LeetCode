0275dc894327c7589108947c47d4ddacf97d3984
ihays <64121529+ihays@users.noreply.github.com>
2021-07-13 10:30:46 -0700

    Update HouseRobber.cpp

--- C++/HouseRobber.cpp
+++ C++/HouseRobber.cpp
@@ -1,24 +1,20 @@
-//Ian Hays
-//06-30-2020
-//https://leetcode.com/problems/house-robber/
-//SC: O(1) TC: O(N)
-//DP - a: maximum amount stashed from previous states. b: pending max amount stashed from previous states. c: max amount stashed pointed to by i. 
-
+/***********************************************************************************************
+Problem      House Robber
+Developer    Ian Hays
+Date         07/13/2021
+URL          https://leetcode.com/problems/house-robber/
+Space        O(1)
+Time         O(N)
+Description  We dp backwards the maximum value a robber can stash. We set each entry to the greatest
+             value between the next house over, or current max. We return the max of the first two
+             houses. 
+************************************************************************************************/
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        if(nums.size() == 0) return 0;
-        if(nums.size() == 1) return nums[0];
-        if(nums.size() == 2) return max(nums[0],nums[1]);
-        int a = nums[0];
-        int b = nums[1]; 
-        int c = INT_MIN;
-        for(int i = 2; i < nums.size(); i++){
-            c = max(c,nums[i] + a);
-            a = max(a,b);
-            b = c;
-        }
-        return max(a,c);
+        for(int i = size(nums)-2; i >= 0; i--)
+            nums[i] = i+2 < size(nums) ? max(nums[i] + nums[i+2], nums[i+1]) : max(nums[i], nums[i+1]);
+        return 1 < size(nums) ? max(nums[0], nums[1]) : nums[0];
     }
 };
 
