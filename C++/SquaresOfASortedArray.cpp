c56331d26be9794a9cbfad5b797f9d4593d518c3
ihays <64121529+ihays@users.noreply.github.com>
2021-07-14 15:22:21 -0700

    Update SquaresOfASortedArray.cpp

--- C++/SquaresOfASortedArray.cpp
+++ C++/SquaresOfASortedArray.cpp
@@ -1,15 +1,25 @@
-//Ian Hays
-//07-09-2020
-//https://leetcode.com/problems/squares-of-a-sorted-array/
-//SC: O(1) TC: O(N*Log(N))
+/***********************************************************************************************
+Problem      Squares of a Sorted Array
+Developer    Ian Hays
+Date         07/14/2021
+URL          https://leetcode.com/problems/squares-of-a-sorted-array/
+Space        O(N)
+Time         O(N)
+Description  Since input nums is in non-increasing order, we want to grab the largest square on
+             the outside, and insert into the array from the end backwards. 
+************************************************************************************************/
 
 class Solution {
 public:
-    vector<int> sortedSquares(vector<int>& A) {
-        for(int i = 0; i < A.size(); i++){
-            A[i]*=A[i];
+    vector<int> sortedSquares(vector<int>& nums) {
+        vector<int> res(nums.size());
+        int l = 0;
+        int r = nums.size()-1;
+
+        for(int i = nums.size()-1; i >= 0; i--){
+            if(abs(nums[l]) > abs(nums[r])) res[i] = nums[l] * nums[l++];
+            else res[i] = nums[r] * nums[r--];
         }
-        sort(A.begin(),A.end());
-        return A;
+        return res;
     }
 };
