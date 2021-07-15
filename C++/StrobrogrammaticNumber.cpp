843beb999828da4d9dc8d135e6d64001ddf84188
ihays <64121529+ihays@users.noreply.github.com>
2021-07-15 14:33:33 -0700

    Update StrobrogrammaticNumber.cpp

--- C++/StrobrogrammaticNumber.cpp
+++ C++/StrobrogrammaticNumber.cpp
@@ -1,7 +1,7 @@
 /***********************************************************************************************
 Problem      Strobogrammatic Number
 Developer    Ian Hays
-Date         03/08/2021
+Date         07/15/2021
 URL          https://leetcode.com/problems/strobogrammatic-number/
 Space        O(1) 
 Time         O(N)
@@ -12,16 +12,9 @@ Description  Checks end points moving towards middle of string to see if they ma
 class Solution {
 public:
     bool isStrobogrammatic(string num) {
-        unordered_map<char,char> umap {
-            {'6', '9'}, 
-            {'9', '6'}, 
-            {'0', '0'}, 
-            {'1', '1'},
-            {'8', '8'}
-        };
-        for(int i = 0; i < num.size(); i++){
-            if(umap[num[i]] != num[num.size()-1-i]) return false;
-        } 
+        unordered_map<char,char> umap = {{'6', '9'},{'9', '6'},{'1', '1'},{'0', '0'},{'8', '8'}};
+        int l = 0, r = size(num)-1;
+        while(l <= r) if(umap[num[l++]] != num[r--]) return false;
         return true;
     }
 };
