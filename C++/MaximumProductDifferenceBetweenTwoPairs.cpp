cac5c1c4e1f2fc60a736cb023374102f17cc4010
ihays <64121529+ihays@users.noreply.github.com>
2021-07-18 19:04:56 -0700

    Update MaximumProductDifferenceBetweenTwoPairs.cpp

--- C++/MaximumProductDifferenceBetweenTwoPairs.cpp
+++ C++/MaximumProductDifferenceBetweenTwoPairs.cpp
@@ -4,14 +4,29 @@ Developer    Ian Hays
 Date         07/18/2021
 URL          https://leetcode.com/problems/maximum-product-difference-between-two-pairs/
 Space        O(1) 
-Time         O(N*LOG(N))
-Description  Sort the array and return the difference between the greatest two and smallest two elements.
+Time         O(N)
+Description  Iterate the array to assign the 2 minimum elements and 2 maximum elements to integer
+             values. Return the difference in products.
 ************************************************************************************************/
 
 class Solution {
 public:
     int maxProductDifference(vector<int>& nums) {
-        sort(begin(nums), end(nums));
-        return nums[size(nums)-1] * nums[size(nums)-2] - nums[0] * nums[1];
+        int min1 = INT_MAX, min2 = INT_MAX, max1 = INT_MIN, max2 = INT_MIN;
+        for(auto num: nums){
+            if(num < min1){
+                min2 = min1;
+                min1 = num;
+            } else if(num < min2){
+                min2 = num; 
+            }    
+            if(num > max1){
+                max2 = max1;
+                max1 = num;
+            } else if(num > max2){
+                max2 = num;
+            }
+        }
+        return max1 * max2 - min1 * min2;
     }
 };
