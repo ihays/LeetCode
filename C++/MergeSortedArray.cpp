262214741a46d095f927cefc2d8481b65e81b2f5
ihays <64121529+ihays@users.noreply.github.com>
2021-07-21 15:51:04 -0700

    Update MergeSortedArray.cpp

--- C++/MergeSortedArray.cpp
+++ C++/MergeSortedArray.cpp
@@ -1,3 +1,21 @@
+/***********************************************************************************************
+Problem      Inorder Successor In BST
+Developer    Ian Hays
+Date         07/21/2021
+URL          https://leetcode.com/problems/merge-sorted-array/
+Space        O(1) 
+Time         O(N*LOG(N))
+Description  Simple use of sort algorithm after moving elements from nums2 to nums1.
+************************************************************************************************/
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        for(int i = m; i < m + n; i++) nums1[i] = nums2[i-m];
+        sort(begin(nums1), end(nums1));
+    }
+};
+
 //Ian Hays
 //01-11-2021
 //https://leetcode.com/problems/merge-sorted-array/
