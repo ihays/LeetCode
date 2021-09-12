270e7e060b8d26288d3531cabafd48028e632e31
ihays <64121529+ihays@users.noreply.github.com>
2021-09-12 15:30:24 -0700

    Update FlipGame.cpp

--- C++/FlipGame.cpp
+++ C++/FlipGame.cpp
@@ -1,12 +1,12 @@
 /***********************************************************************************************
 Problem      Flip Game
 Developer    Ian Hays
-Date         09/08/2021
+Date         09/12/2021
 URL          https://leetcode.com/problems/flip-game/
 Space        O(N) 
 Time         O(N)
-Description Traverse through currentState flipping consecutive '-' to '+' and pushing back the 
-            result and then backtracking. 
+Description  Traverse through currentState flipping consecutive '-' to '+' and pushing back the 
+             result and then backtracking. 
 ************************************************************************************************/
 
 class Solution {
