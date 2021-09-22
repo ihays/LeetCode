a3ca529db297bb0e4557a763b6096791e1317241
ihays <64121529+ihays@users.noreply.github.com>
2021-09-22 15:05:56 -0700

    Update MaximumLengthOfAConcatenatedStringWithUniqueCharacters.cpp.cpp

--- C++/MaximumLengthOfAConcatenatedStringWithUniqueCharacters.cpp.cpp
+++ C++/MaximumLengthOfAConcatenatedStringWithUniqueCharacters.cpp.cpp
@@ -1,22 +1,34 @@
-//Ian Hays
-//01-23-2021
-//https://leetcode.com/problems/maximum-length-of-a-concatenated-string-with-unique-characters/
-//SC: O(N^2) TC: O(N^2)
+/***********************************************************************************************
+Problem      Maximum Length of a Concatenated String with Unique Characters
+Developer    Ian Hays
+Date         09/22/2021
+URL          https://leetcode.com/problems/maximum-length-of-a-concatenated-string-with-unique-characters/
+Space        O(1) 
+Time         O(N)
+Description  We use bitset to determine if string is unique, and then store unique combinations in
+             a dp vector to find maximum length.
+************************************************************************************************/
 
 class Solution {
 public:
     int maxLength(vector<string>& arr) {
-        return dfs(arr, "", 0);
-    }
-    
-    int dfs(vector<string>& arr, string s, int index){
-        bitset<26> a;
-        for(auto ch: s) a.set(ch - 'a');
-        if(a.count() != s.size()) return 0;
+        vector<bitset<26>> dp = {bitset<26>()};
+        int res = 0;
         
-        int res = s.size();
-        for(int i = index; i < arr.size(); i++){
-            res = max(res, dfs(arr, s + arr[i], i+1));
+        for(auto str: arr){
+            bitset<26> a;
+            for(auto ch: str){
+                a.set(ch-'a');
+            }
+            int n = a.count();
+            if(n < size(str)) continue;
+            for(int i = size(dp)-1; i >= 0; i--){
+                bitset<26> b = dp[i];
+                if((a & b).any()) continue;
+                bitset<26> c(a | b);
+                dp.push_back(c);
+                res = max(res, (int)c.count());
+            }
         }
         return res;
     }
