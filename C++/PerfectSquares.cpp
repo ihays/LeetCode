73cd69c8417e4e4e2ed1109ac01114969c677369
ihays <64121529+ihays@users.noreply.github.com>
2021-10-14 14:41:00 -0700

    Update PerfectSquares.cpp

--- C++/PerfectSquares.cpp
+++ C++/PerfectSquares.cpp
@@ -1,33 +1,41 @@
-//Ian Hays
-//06-29-2020
-//https://leetcode.com/problems/perfect-squares
-//SC: O(N^2) TC: O(N^2)
-//Knapsack DP 2D
-
+/***********************************************************************************************
+Problem      Perfect Squares
+Developer    Ian Hays
+Date         10/14/2021
+URL          https://leetcode.com/problems/perfect-squares/
+Space        O(N) 
+Time         O(N*LOG(N))
+Description  Iterative DP allows us to build the minimum squares for each entry leading up to n. 
+             Recursive starts with grabbing the largest square, and divides its quantity to determine
+             minimum squares. 
+************************************************************************************************/
+//Iterative DP
 class Solution {
 public:
     int numSquares(int n) {
-        vector<vector<int>> knapsack;
-        vector<int> squares;
-        for(int i = 0; i*i <= n; i++){
-            knapsack.push_back(vector<int>(n+1));
-            squares.push_back(i*i);
-        }
-       
-        int m = squares.size();
-       
-        for(int i = 0; i < n+1; i++){
-            knapsack[0][i] = INT_MAX;
-        }
-       
-        for(int i = 1; i < m; i++){
-            for(int j = 1; j < n+1; j++){
-                knapsack[i][j] = knapsack[i-1][j];
-                if(j >= squares[i]){
-                    knapsack[i][j] = min(knapsack[i][j], 1 + knapsack[i][j-squares[i]]);
-                }
+        vector<int> dp(n+1, 0);
+        for(int i = 1; i <= n; i++){
+            int squares = INT_MAX;
+            for(int j = 1; j*j <= i; j++){
+                squares = min(squares, dp[i-j*j] + 1);
             }
+            dp[i] = squares;
         }
-        return knapsack[m-1][n];
+        return dp[n];
     }
+};
+
+//Recursive
+class Solution {
+public:
+    int numSquares(int n, int count = 0) {
+        if(n == 0) return count;
+        int res = INT_MAX;
+        for(int i = floor(sqrt(n)); i > 0; i--){
+            int square = i*i;            
+            res = min(res, numSquares(n%square, count + (n/square)));
+        }
+        return res;
+    }
+    
 };
