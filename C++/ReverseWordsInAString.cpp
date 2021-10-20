a21fbb7875c92a8870c98355c381aa9cc16d3ac5
ihays <64121529+ihays@users.noreply.github.com>
2021-10-19 20:22:22 -0700

    Update ReverseWordsInAString.cpp

--- C++/ReverseWordsInAString.cpp
+++ C++/ReverseWordsInAString.cpp
@@ -1,47 +1,30 @@
-//Ian Hays
-//07-09-2020
-//https://leetcode.com/problems/reverse-words-in-a-string/
-//SC: O(1) TC: O(N)
-//constant memory, in place reverse words in a string.
+/***********************************************************************************************
+Problem      Reverse Words in a String
+Developer    Ian Hays
+Date         10/19/2021
+URL          https://leetcode.com/problems/reverse-words-in-a-string/
+Space        O(N) 
+Time         O(N)
+Description  Move words from the string to a string stream to place into the stack. We pop out of
+             the stack into a result string.
+************************************************************************************************/
 
 class Solution {
 public:
     string reverseWords(string s) {
-        if(s.empty()) return s;
-        
-        //remove leading spaces
-        while(!s.empty() && isspace(s[0])) s.erase(s.begin());
-        reverse(s,0,s.length()-1);
-        
-        //remove end spaces
-        while(!s.empty() && isspace(s[0])) s.erase(s.begin());
-        
-        //remove duplicate spaces
-        for(int i = 1; i < s.length(); i++){
-            if(isspace(s[i-1]) && isspace(s[i])){
-                s.erase(s.begin()+i);
-                i--;
-            }
+        istringstream iss(s);
+        stack<string> stack;
+        while(iss){
+            string word;
+            iss >> word;
+            if(word != "") stack.push(word);            
         }
-        
-        int l = 0;
-        int r = s.length()-1;
-        while(r != string::npos){
-            r = s.find(' ',l);
-            reverse(s,l,r-1);
-            l = r+1;
-        }  
-
-        return s;
-    }
-    
-    string reverse(string& s, int l, int r){
-        if(r == -2) r = s.size()-1;
-        while(l < r){
-            swap(s[l],s[r]);
-            l++;
-            r--;
+        string res;
+        while(!stack.empty()){
+            res += stack.top() + " ";
+            stack.pop();
         }
-        return s;
+        res.pop_back();
+        return res;
     }
 };
