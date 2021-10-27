9ee5ed91741e1f1f1dde6dbbf9ea567bb85846c0
ihays <64121529+ihays@users.noreply.github.com>
2021-10-26 20:17:32 -0700

    Update SortColors.cpp

--- C++/SortColors.cpp
+++ C++/SortColors.cpp
@@ -1,20 +1,23 @@
-//Ian Hays
-//07-19-2020
-//https://leetcode.com/problems/sort-colors/
-//SC: O(1) TC: O(N)
-
+/***********************************************************************************************
+Problem      132 Pattern
+Developer    Ian Hays
+Date         10/26/21
+URL          https://leetcode.com/problems/sort-colors/
+Space        O(1) 
+Time         O(N)
+Description  We track the frequency of colors in an array and then push them back into a cleared
+             input vector to return the result.
+************************************************************************************************/
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int counter[3] = {0,0,0};
-        for(auto num: nums){
-            counter[num]++;
-        }
+        int colors[3] = {};
+        for(int num: nums) colors[num]++;
         nums.clear();
         for(int i = 0; i < 3; i++){
-            while(counter[i]--){
+            while(colors[i]--){
                 nums.push_back(i);
             }
-        }        
+        }
     }
 };
