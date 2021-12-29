88bc952227dfd6bd4d3e4c56bdae7f78fe3b3651
ihays <64121529+ihays@users.noreply.github.com>
2021-12-28 18:43:39 -0800

    Update SearchInsertPosition.cpp

--- C++/SearchInsertPosition.cpp
+++ C++/SearchInsertPosition.cpp
@@ -1,25 +1,21 @@
-//Ian Hays
-//12-17-20
-//https://leetcode.com/problems/search-insert-position/
-//SC: O(1) TC: O(LOG(N))
-
+/***********************************************************************************************
+Problem      Search Insert Position
+Developer    Ian Hays
+Date         12/28/2021
+URL          https://leetcode.com/problems/search-insert-position/
+Space        O(1)
+Time         O(LOG(N))
+Description  Binary search for insert position of value in sorted list. 
+************************************************************************************************/
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        int l = 0; 
-        int r = nums.size()-1;
-        int m = (r - l)/2;
-        
+        int l = 0, r = size(nums)-1, m = (r-l)/2;
         while(l <= r){
-            if(nums[m] < target){
-                l = m+1;
-            } else {
-                r = m-1;
-            }
+            if(nums[m] < target) l = m+1;
+            else r = m-1;
             m = l + (r - l)/2;
-            
         }
-        
         return m;
     }
 };
