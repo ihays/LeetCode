2f815abe3ba9a44e22ab2b5f9b0a0772d17c3e38
ihays <64121529+ihays@users.noreply.github.com>
2022-02-12 20:13:34 -0800

    Update Subsets.cpp

--- C++/Subsets.cpp
+++ C++/Subsets.cpp
@@ -1,14 +1,19 @@
-//Ian Hays
-//07-11-2020
-//https://leetcode.com/problems/subsets/
-//SC: O(N^2) TC: O(N^2)
+/***********************************************************************************************
+Problem      Subsets
+Developer    Ian Hays
+Date         02/12/2022
+URL          https://leetcode.com/problems/subsets/
+Space        O(2^N) 
+Time         O(2^N)
+Description  DFS through each word as a set until end word is found. 
+************************************************************************************************/
 
 class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> res(1, vector<int>());
-        for(int i = 0; i < nums.size(); i++){
-            int m = res.size();
+        vector<vector<int>> res (1, vector<int>());
+        for(int i = 0; i < size(nums); i++){
+            int m = size(res);
             for(int j = 0; j < m; j++){
                 vector<int> tmp = res[j];
                 tmp.push_back(nums[i]);
