f6307721c8540c1c3ffef39aa4a320902a84cc16
ihays <64121529+ihays@users.noreply.github.com>
2022-02-28 10:48:04 -0800

    Update LargestRectangleInHistogram.cpp

--- C++/LargestRectangleInHistogram.cpp
+++ C++/LargestRectangleInHistogram.cpp
@@ -5,38 +5,26 @@ Date         02/27/2022
 URL          https://leetcode.com/problems/largest-rectangle-in-histogram/
 Space        O(N)
 Time         O(N)
-Description  Store the right and left local maximas to determine the area. Return largest area. 
-************************************************************************************************/
+Description  Using a stack to mark indexes of local minima/maxima to generate the largest rectangle. 
+************************************************************************************************/  
 
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
-        int n = size(heights);
-        vector<int> lLocalMaxima(n, 0);
-        vector<int> rLocalMaxima(n, 0);
-        lLocalMaxima[0] = -1;
-        rLocalMaxima[n-1] = n;
+        stack<int> s;
+        heights.push_back(0);
         
-        for(int i = 1; i < n; i++){
-            int maxima = i-1;
-            while(maxima >= 0 && heights[maxima] >= heights[i]){
-                maxima = lLocalMaxima[maxima];
-            }
-            lLocalMaxima[i] = maxima;
-        }
-        
-        for(int i = n-2; i >= 0; i--){
-            int maxima = i+1;
-            while(maxima < n && heights[maxima] >= heights[i]){
-                maxima = rLocalMaxima[maxima];
-            }
-            rLocalMaxima[i] = maxima;
-        }
         int res = 0;
-        for(int i = 0; i < n; i++){
-            res = max(res, heights[i] * (rLocalMaxima[i] - lLocalMaxima[i] - 1));
+        for(int i = 0; i < size(heights); i++){
+            
+            while(!s.empty() && heights[i] < heights[s.top()]){
+                int height = heights[s.top()];
+                s.pop();
+                int ran = s.empty() ? -1 : s.top();
+                res = max(res, height * (i - ran - 1));
+            }
+            s.push(i);
         }
         return res;
-        
     }
 };
