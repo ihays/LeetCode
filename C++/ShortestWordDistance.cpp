351fb769aa94d2fcce75f0f7cd4701e2f4fa9f0e
ihays <64121529+ihays@users.noreply.github.com>
2022-06-06 07:46:51 -0700

    Update ShortestWordDistance.cpp

--- C++/ShortestWordDistance.cpp
+++ C++/ShortestWordDistance.cpp
@@ -12,19 +12,11 @@ Description  Store locations of words and grab the minimum distance between them
 class Solution {
 public:
     int shortestDistance(vector<string>& wordsDict, string word1, string word2) {
-        int location1 = INT_MAX;
-        int location2 = INT_MAX;
-        int res = INT_MAX;
-        for(int i = 0; i < wordsDict.size(); i++){
-            string word = wordsDict[i];
-            if(word == word1){
-                location1 = i;
-            } else if(word == word2){
-                location2 = i;
-            } else {
-                continue;
-            }
-            if(location1 != INT_MAX && location2 != INT_MAX) res = min(res,abs(location2 - location1));
+        int i1 = -1, i2 = -1, res = INT_MAX;
+        for(int i = 0; i < size(wordsDict); i++){
+            if(wordsDict[i] == word1) i1 = i;    
+            if(wordsDict[i] == word2) i2 = i;
+            if(i1 != -1 && i2 != -1) res = min(res, abs(i2-i1));
         }
         return res;
     }
