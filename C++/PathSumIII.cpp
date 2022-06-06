099650e01d93aa732f67c7fac34f1634ef43c2af
ihays <64121529+ihays@users.noreply.github.com>
2022-06-06 11:28:40 -0700

    Update PathSumIII.cpp

--- C++/PathSumIII.cpp
+++ C++/PathSumIII.cpp
@@ -1,13 +1,25 @@
 /***********************************************************************************************
 Problem      Path Sum III
 Developer    Ian Hays
-Date         03/23/2021
+Date         06/06/2022
 URL          https://leetcode.com/problems/path-sum-iii/
 Space        O(1) 
 Time         O(N^2)
-Description  DFS on each node the sums of all children. Any sum path equal to 0 increments the result.
+Description  DFS on each node the sums of all children. Subtract from target sum and any sum path 
+             equal to 0 increments the result.
 ************************************************************************************************/
 
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -22,17 +34,16 @@ Description  DFS on each node the sums of all children. Any sum path equal to 0 
 class Solution {
 public:
     int res = 0;
-    
     int pathSum(TreeNode* root, int targetSum) {
         if(root){
             dfs(root, targetSum);
             pathSum(root->left, targetSum);
-            pathSum(root->right, targetSum);
+            pathSum(root->right, targetSum);    
         }
         return res;
     }
     
-    void dfs(TreeNode* root, int sum){
+    void dfs(TreeNode* root, long sum){
         if(!root) return;
         if(!(sum -= root->val)) res++;
         dfs(root->left, sum);
