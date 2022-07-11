98fe74a310c3d12b7b69b6a2cad4b1efdf1e78a2
ihays <64121529+ihays@users.noreply.github.com>
2022-07-11 15:18:02 -0700

    Update FindPivotIndex.cpp

--- C++/FindPivotIndex.cpp
+++ C++/FindPivotIndex.cpp
@@ -1,7 +1,7 @@
 /***********************************************************************************************
 Problem      Find Pivot Index
 Developer    Ian Hays
-Date         06/22/2022
+Date         07/11/2022
 URL          https://leetcode.com/problems/find-pivot-index/
 Space        O(1) 
 Time         O(N)
@@ -11,14 +11,14 @@ Description  track left and right sum of index. Return index when they are equal
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
-        int rightSum = 0, leftSum = 0;
+        int leftSum = 0, rightSum = 0;
         
         for(auto num: nums) rightSum += num;
         
         for(int i = 0; i < size(nums); i++){
             rightSum -= nums[i];
-            if(i > 0) leftSum += nums[i-1];
-            if(rightSum == leftSum) return i;
+            if(leftSum == rightSum) return i; 
+            leftSum += nums[i];
         }
         return -1;
     }
